8a5ca7bdb7932c94d29892ffd6e5950e3e5d69cf
Lakshan Ruwinda <112312002+shan-ganegoda@users.noreply.github.com>
2025-05-18 01:33:12 +0530

    build: Employee Form Ui

--- client/src/app/modules/employee/employee.component.ts
+++ client/src/app/modules/employee/employee.component.ts
@@ -1,11 +1,95 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Breadcrumb} from 'primeng/breadcrumb';
+import {MenuItem} from 'primeng/api';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FloatLabel} from 'primeng/floatlabel';
+import {InputText} from 'primeng/inputtext';
+import {Select} from 'primeng/select';
+import {DatePicker} from 'primeng/datepicker';
+import {Textarea} from 'primeng/textarea';
+import {Button, ButtonModule} from 'primeng/button';
+import {ButtonGroup} from 'primeng/buttongroup';
+
+interface Gender {
+  name: string;
+  id: string;
+}
+
+interface Designation {
+  name: string;
+  id: string;
+}
+
+interface EmployeeStatus {
+  name: string;
+  id: string;
+}
+
+interface EmployeeType {
+  name: string;
+  id: string;
+}
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [
+    Breadcrumb,
+    MatGridList,
+    MatGridTile,
+    ReactiveFormsModule,
+    FloatLabel,
+    InputText,
+    Select,
+    DatePicker,
+    Textarea,
+    Button,
+    ButtonModule,
+    ButtonGroup
+  ],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit{
 
+  items: MenuItem[] | undefined;
+
+  genders: Gender[] = [{ name: 'New York', id: 'NY' },
+    { name: 'Rome', id: 'RM' },];
+
+  designations: Designation[] = [{ name: 'New York', id: 'NY' },
+    { name: 'Rome', id: 'RM' },];
+
+  employeetypes: Designation[] = [{ name: 'New York', id: 'NY' },
+    { name: 'Rome', id: 'RM' },];
+
+  employeestatuses: Designation[] = [{ name: 'New York', id: 'NY' },
+    { name: 'Rome', id: 'RM' },];
+
+
+
+
+  home: MenuItem | undefined;
+  imageempurl: string = "";
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Main' },
+      { label: 'Employee' },
+    ];
+
+    this.home = { icon: 'pi pi-home', routerLink: '../home' };
+  }
+
+
+  selectImage($event: Event) {
+
+  }
+
+  clearImage() {
+
+  }
 }
