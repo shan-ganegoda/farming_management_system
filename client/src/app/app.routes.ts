bc22a598a33cdb2a9cb2316accbad268ce9f003e
Lakshan Ruwinda <112312002+shan-ganegoda@users.noreply.github.com>
2025-05-17 22:24:26 +0530

    build: Sidenav Updated

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import {LoginComponent} from './shared/login/login.component';
 import {MainwindowComponent} from './shared/mainwindow/mainwindow.component';
 import {HomeComponent} from './shared/home/home.component';
 import {authenticationGuard} from './utils/guards/authentication.guard';
+import {EmployeeComponent} from './modules/employee/employee.component';
+import {UserComponent} from './modules/user/user.component';
+import {AccessComponent} from './modules/access/access.component';
 
 export const routes: Routes = [
   { path: "login", component: LoginComponent, title: 'Login' },
@@ -13,6 +16,10 @@ export const routes: Routes = [
     canActivate:[authenticationGuard],
     children: [
       { path: "home", component: HomeComponent, title: 'Home' },
+
+      { path: "employee", component: EmployeeComponent, title: 'Employee' },
+      { path: "user", component: UserComponent, title: 'User' },
+      { path: "access", component: AccessComponent, title: 'Access' },
     ]
   }
 ];
---
+++ client/src/app/modules/access/access.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-access',
+  imports: [],
+  templateUrl: './access.component.html',
+  styleUrl: './access.component.scss'
+})
+export class AccessComponent {
+
+}
